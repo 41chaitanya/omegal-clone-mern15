b583f824c7be6d9ddcd130a3f83b8b9188d2122c
41chaitanya <chaitanya4141sharma@gmail.com>
2026-03-11 19:45:25 +0530

    config: all clients wiht socket  ,  prepared: server for all clients , done:message  sending  no ui , update: the email and job resources

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,15 @@ app.get("/health",(req,res)=>{
 
 io.on('connection', (socket) => {
   console.log('a clint connected',socket.id);
+  socket.on("sender",(senderData)=>{
+        const{targetId,message}=senderData
+        console.log(targetId ,message)
+        io.to(targetId).emit("receiver",{
+            sender:socket.id,
+            message:message
+        })
+
+  })
 });
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,58 @@ const socket = io("http://localhost:9000");
 
 function App() {
   const [socketID, setSocketID] = useState("");
+  const [targetId,setTargetId]=useState("")
+  const [message,setMessage]=useState("")
+  const [allMessage,setAllMessage]=useState([])
+  const sendMessage =()=>{
+    console.log("ruk ja bhej raha hu")
+
+    socket.emit("sender",{
+      targetId:targetId,
+      message:message
+    })
+  }
   useEffect(() => {
     socket.on("connect", () => {
       console.log(socket.id); // x8WIv7-mJelg7on_ALbx
       setSocketID(socket.id);
     });
+    socket.on("receiver",(receiverData)=>{
+        setAllMessage((prev)=>[...prev,receiverData])
+    })
   }, []);
 
   return (
     <>
-      {socketID}
-      hello
+
+      <div className="outer">
+        <div className="chatSection">
+          <div className="userHeader">{socketID}</div>
+          <div className="chatArea">
+            {
+              allMessage.map((msg,index)=>(<div key={index}>
+                  "message": {msg.message}
+
+              </div>))
+            }
+          </div>
+          <div className="inputArea">
+            <input type="text" placeholder="enter the target id" value={targetId} onChange={(e)=>setTargetId(e.target.value)} />
+            <input type="text" placeholder="enter the message" value={message} onChange={(e)=>setMessage(e.target.value)} />
+            <button onClick={sendMessage}>send now</button>
+
+          </div>
+
+
+
+
+          {/* chat kre ge ya */}
+        </div>
+        <div className="peerConnection">
+          {/* vdo */}
+        </div>
+      </div>  
+      
     </>
   );
 }
